9d2009546d8f2f564915b2ca1114f99cb5b9e66b
Mohammad <usermp76@gmail.com>
2023-04-26 16:09:25 +0330

    Add ServiceProvider class

---
+++ src/MultiSmsServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Usermp\MultiSms;
+
+use Illuminate\Support\ServiceProvider;
+
+class MultiSmsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/multisms.php', 'multisms'
+        );
+
+        $this->app->singleton(MultiSms::class, function () {
+            return new MultiSms();
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/multisms.php' => config_path('multisms.php'),
+        ]);
+    }
+}
